58e329bf367803917e190961cf7404ab79a616d0
Martin Sakaliev <martinsakaliev12@gmail.com>
2026-01-27 17:08:24 +0200

    To input the data

--- client_angular/src/app/components/users-list-component/users-list-component.ts
+++ client_angular/src/app/components/users-list-component/users-list-component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { AvatarComponent } from '../avatar-component/avatar-component';
+import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user.model';
+import { UserService } from '../../core/services/user.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-users-list-component',
-  imports: [AvatarComponent],
+  imports: [ AsyncPipe, AvatarComponent],
   templateUrl: './users-list-component.html',
   styleUrl: './users-list-component.css',
 })
 export class UsersListComponent {
-  //todo add load button to work
+  users$:Observable<User[]>|null=null
+  constructor(private userService:UserService){
+
+  }
+
+  loadUsers(){
+    this.users$ = this.userService.getUsers()
+  }
   
 }
