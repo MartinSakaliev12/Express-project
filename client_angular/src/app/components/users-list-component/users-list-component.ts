b5f9a16ac90f9f0e94395d31218e10a89793343e
Martin Sakaliev <martinsakaliev12@gmail.com>
2026-01-27 19:28:28 +0200

    Add formModule

--- client_angular/src/app/components/users-list-component/users-list-component.ts
+++ client_angular/src/app/components/users-list-component/users-list-component.ts
@@ -4,17 +4,22 @@ import { Observable } from 'rxjs';
 import { User } from '../../shared/models/user.model';
 import { UserService } from '../../core/services/user.service';
 import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-list-component',
-  imports: [ AsyncPipe, AvatarComponent],
+  imports: [ FormsModule,AsyncPipe, AvatarComponent],
   templateUrl: './users-list-component.html',
   styleUrl: './users-list-component.css',
 })
 export class UsersListComponent {
   //todo create loading state
+  //todo add messages on error
   users$:Observable<User[]>|null=null
   isAdding:boolean = false
+
+  inputName:string = ""
+  inputEmail:string = ""
   constructor(private userService:UserService){
 
   }
@@ -22,10 +27,14 @@ export class UsersListComponent {
   loadUsers(){
     this.users$ = this.userService.getUsers()
   }
-  addUser(){
+  startAddingUser(){
     this.isAdding = true;
+
   }
   stopAddingUser(){
     this.isAdding = false;
+  }
+  addUser(){
+    
   }
 }
