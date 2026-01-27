300d716e18a0fb22774d1ef8f7162340009fac1b
Martin Sakaliev <martinsakaliev12@gmail.com>
2026-01-27 17:08:51 +0200

    Add service

--- client_angular/src/app/core/services/user.service.ts
+++ client_angular/src/app/core/services/user.service.ts
@@ -1,6 +1,19 @@
+import { Observable } from "rxjs";
+import { User } from "../../shared/models/user.model";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 
-
+@Injectable({
+    providedIn:'root'
+})
 export class UserService{
-    getUsers():
+    private url = "http://localhost:3000/users"
+    constructor(private httpClient:HttpClient){
+
+    }
+
+    getUsers():Observable<User[]>{
+        return this.httpClient.get<User[]>(this.url)
+    }
 }
