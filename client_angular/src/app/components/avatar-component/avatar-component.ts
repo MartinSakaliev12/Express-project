1284cf568c395cfe726591f8623a1a10f8a109ee
Martin Sakaliev <martinsakaliev12@gmail.com>
2026-01-27 20:18:14 +0200

    Add edit view

--- client_angular/src/app/components/avatar-component/avatar-component.ts
+++ client_angular/src/app/components/avatar-component/avatar-component.ts
@@ -1,21 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../shared/models/user.model';
 import { UserUtils } from '../../core/utils/user.utils';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: '[app-user-row]',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './avatar-component.html',
   styleUrl: './avatar-component.css',
 })
 export class AvatarComponent {
+  isEditing:boolean = false;
   @Input() user:User|null = null
+
+  newName:string|undefined = ""
+  newEmail:string|undefined = ""
+
+  
   userUtils = new UserUtils()
+  
   get initial():string{
     return this.userUtils.getInitials(this.user?.name,this.user?.email)
   }
   get backGroundColor():string{
     return this.userUtils.getColors(this.initial)
   }
-
+  stratEditing():void{
+    this.newName = this.user?.name
+    this.newEmail = this.user?.email
+    this.isEditing=true;
+  }
 }
